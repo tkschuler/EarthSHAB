7b75a28376b5b52d01c9569cd186296cfe2559cf
Tristan Schuler <tkschuler@yahoo.com>
2024-07-11 12:44:50 -0400

    Changed Config file to run by default with SHAB14-V

--- config_earth.py
+++ config_earth.py
@@ -36,7 +36,7 @@ balloon_properties = dict(
 #SHAB14-V Example for EarthSHAB software
 forecast_start_time =  "2022-08-22 12:00:00" # Forecast start time, should match a downloaded forecast in the forecasts directory
 start_time = datetime.fromisoformat("2022-08-22 14:01:00") # Simulation start time. The end time needs to be within the downloaded forecast
-balloon_trajectory = "balloon_data/SHAB12V-APRS.csv"  # Only Accepting Files in the Standard APRS.fi format for now
+balloon_trajectory = "balloon_data/SHAB14V-APRS.csv"  # Only Accepting Files in the Standard APRS.fi format for now
 
 #Hawaii
 #forecast_start_time = "2023-04-18 00:00:00" # Forecast start time, should match a downloaded forecast
@@ -69,7 +69,7 @@ netcdf_era5 = dict(
     #filename = "SHAB3V_era_20201120_20201121.nc", #SHAB3
     #filename = "SHAB5V-ERA5_20210512_20210513.nc", #SHAB5V
     #filename = "shab10_era_2022-04-09to2022-04-10.nc", #SHAB10V
-    filename = "SHAB12V_ERA5_20220822_20220823.nc", #SHAB12/13/14/15V
+    filename = "SHAB14V_ERA5_20220822_20220823.nc", #SHAB12/13/14/15V
     #filename = "hawaii-ERA5-041823.nc",
     resolution_hr = 1
     )
